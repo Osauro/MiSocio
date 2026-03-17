ea9b444ad0186c7b7dbb1acaed9c3e99d18b635b
Osauro <acxel.q17@hotmail.com>
2026-03-17 09:33:56 -0400

    fix: corregir enlace del icono prestamo-cart en header

--- resources/views/livewire/prestamo-cart.blade.php
+++ resources/views/livewire/prestamo-cart.blade.php
@@ -1,9 +1,7 @@
 <div x-data x-on:actualizar-badge-prestamo.window="$wire.actualizarContador()">
-    @if($prestamoPendienteId)
-        <a href="{{ route('prestamo', ['prestamoId' => $prestamoPendienteId]) }}" class="cart-icon-link position-relative" title="Préstamo en proceso">
-    @else
-        <a href="{{ route('prestamos') }}" class="cart-icon-link position-relative" title="Préstamos">
-    @endif
+    <a href="{{ $prestamoPendienteId ? route('prestamo', ['prestamoId' => $prestamoPendienteId]) : route('prestamos') }}"
+       class="cart-icon-link position-relative"
+       title="{{ $prestamoPendienteId ? 'Préstamo en proceso' : 'Préstamos' }}">
         <i class="fa-solid fa-handshake fa-lg"></i>
         @if($cantidadItems > 0)
             <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
